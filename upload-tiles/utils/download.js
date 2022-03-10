b0526727a85fbd7e289e03bbd87541a6da458188
Artem Linenko <artem.linenko@dataart.com>
2022-03-10 14:06:10 +0200

    Handle response.statusCode

--- upload-tiles/utils/download.js
+++ upload-tiles/utils/download.js
@@ -3,11 +3,7 @@ const fs = require("fs");
 const { tilesDestDirectory } = require("../constants");
 
 const download = (resource, locationParams, cb = () => {}) => {
-  const {
-    fileExt,
-    downloadUrlPatter,
-    name: resourceName,
-  } = resource;
+  const { fileExt, downloadUrlPatter, name: resourceName } = resource;
   const { x, y, z } = locationParams;
   const dir = `./${tilesDestDirectory}/${resourceName}/${z}/${x}`;
   const dest = `${dir}/${y}.${fileExt}`;
@@ -18,39 +14,49 @@ const download = (resource, locationParams, cb = () => {}) => {
   //if file is there - skip
   if (fs.existsSync(dest)) {
     return new Promise((resolve) => {
-      resolve({resource, coords: { x, y, z }});
-    })
+      resolve({ resource, coords: { x, y, z } });
+    });
   }
 
   const downloadUrl = downloadUrlPatter(locationParams);
 
   return new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(dest);
-
     console.log(`Downloading file by URL ${downloadUrl} to dest ${dest}`);
 
-    https.get(
-      downloadUrl,
-      {
-        timeout: 5000,
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+    https
+      .get(
+        downloadUrl,
+        {
+          timeout: 5000,
+          headers: {
+            "User-Agent":
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+          },
         },
-      },
-      function (response) {
-        response.pipe(file);
-        file.on("finish", function () {
-          file.close(cb);
-          resolve({resource, coords: { x, y, z }});
-        });
-        file.on("error", function (err) {
-          reject(err);
-        });
-      }
-    ).on('error', (err) => {
-      reject(err);
-    });
+        function (response) {
+          const { statusCode } = response;
+          if (statusCode !== 200) {
+            return reject({
+              downloadUrl,
+              dest,
+              statusCode,
+            });
+          }
+          const file = fs.createWriteStream(dest);
+
+          response.pipe(file);
+          file.on("finish", function () {
+            file.close(cb);
+            resolve({ resource, coords: { x, y, z } });
+          });
+          file.on("error", function (err) {
+            reject(err);
+          });
+        }
+      )
+      .on("error", (err) => {
+        reject(err);
+      });
   });
 };
 
